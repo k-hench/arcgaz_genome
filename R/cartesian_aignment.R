8e5c72a60d191a0b309f26290673687939029aef
k-hench <khench@posteo.net>
2023-04-21 17:31:13 +0200

    cartesian alignment plot

--- R/cartesian_aignment.R
+++ R/cartesian_aignment.R
@@ -3,6 +3,8 @@ library(ggforce)
 library(prismatic)
 library(here)
 library(glue)
+library(patchwork)
+library(ggtext)
 source(here("R/plot_defaults.R"))
 clr_genome = "gray50"
 
@@ -91,19 +93,20 @@ sizes_18 <- sizes |>
 psl <- get_psl(glue("slim_{genomes[[2]]}_on_{genomes[[1]]}.psl.gz"))
 
 n_alignments <- 20
+align_min_length <- 0.2e6
 
 psl_filtered <- psl |>
   filter(tName %in% sizes_18$chr,
          qName %in% sizes_18$chr) |>
   arrange(-tSize) |>
-  # filter(tSize > align_min_length) |>
-  group_by(tName) |>
-  mutate(in_top_n_t = row_number() < n_alignments) |>
-  ungroup() |>
-  group_by(qName) |>
-  mutate(in_top_n_q = row_number() < n_alignments) |>
-  ungroup() |>
-  filter(in_top_n_t | in_top_n_q) |>
+  filter(tSize > align_min_length) |>
+  # group_by(tName) |>
+  # mutate(in_top_n_t = row_number() < n_alignments) |>
+  # ungroup() |>
+  # group_by(qName) |>
+  # mutate(in_top_n_q = row_number() < n_alignments) |>
+  # ungroup() |>
+  # filter(in_top_n_t | in_top_n_q) |>
   left_join(sizes_18 |> select(tName = chr, tg_start = start, t_eo = eo)) |>
   left_join(sizes_18 |> select(qName = chr, qg_start = start, q_eo = eo)) |>
   mutate(ymin_start = tStart + tg_start,
@@ -119,7 +122,7 @@ psl_diag <- psl_filtered |>
 genome_width <- .04
 skip <- .025
 
-psl_diag |>
+p1 <- psl_diag |>
   ggplot() +
   geom_diagonal_wide(aes(x = y, y = x, group = group,
                          color = factor(dir),
@@ -142,11 +145,57 @@ psl_diag |>
   # coord_polar() +
   # scale_x_continuous(limits = c(0,2.59e9)) +
   # scale_y_continuous(limits = c(-1.2,1.2)) +
-  coord_cartesian(ylim = c(-.45, 1.4)) +
+  scale_x_continuous(expand = c(0, 0)) +
+  coord_cartesian(ylim = c(-.15, 1.4),
+                  xlim = c(-2e7, max(sizes_18$end_with_skip)),
+                  clip = "off") +
   scale_color_manual(values = clrs |> clr_alpha(.3)) +
   theme_void(base_family = fnt_sel) +
   theme(legend.position = "none")
 
+p2 <- psl |>
+  ggplot(aes(x = log10(tSize))) +
+  geom_density(color = clrs[2],
+               fill = clr_alpha(clrs[2]))+
+  geom_vline(xintercept = log10(min(psl_filtered$tSize)),
+             color = clrs[2], linetype = 3) +
+  scale_x_continuous(glue("alignment size log<sub>10</sub>(bp)<br>{round(range(psl$tSize) / c(1, 1e6), digits = 2) %>% str_c(' ',.,c('bp ', 'Mb '), collapse = '–')}, n: {nrow(psl)}")) +
+  coord_cartesian(expand = 0) +
+  theme_minimal(base_family = fnt_sel) +
+  theme(axis.title.y = element_blank(),
+        axis.text.y = element_blank(),
+        axis.line.x = element_line(color = clr_genome),
+        panel.grid = element_blank(),
+        axis.title.x = element_markdown(lineheight = 1.5))
+
+p3 <- psl_filtered |>
+  ggplot(aes(x = tSize * 1e-6)) +
+  geom_density(color = clrs[2],
+               fill = clr_alpha(clrs[2])) +
+  geom_vline(xintercept = min(psl_filtered$tSize) * 1e-6,
+             color = clrs[2], linetype = 3) +
+  scale_x_continuous(glue("alignment size (Mb)<br>{sprintf('%.2f',range(psl_filtered$tSize)/1e6) %>% str_c(' ',.,'Mb ', collapse = '–')}, n: {nrow(psl_filtered)}")) +
+  coord_cartesian(expand = 0) +
+  theme_minimal(base_family = fnt_sel) +
+  theme(axis.title.y = element_blank(),
+        axis.text.y = element_blank(),
+        axis.line.x = element_line(color = clr_genome),
+        panel.grid = element_blank(),
+        axis.title.x = element_markdown(lineheight = 1.5))
+
+p1 +
+  ( p2/ p3 + plot_layout(heights = c(1,1))) +
+  plot_annotation(tag_levels = 'a') +
+  plot_layout(widths = c(1, .2)) &
+  theme(text = element_text(family = fnt_sel))
+
+scl <- .9
+ggsave("img/arcgaz_a2_zalcal.pdf",
+       width = 16 * scl,
+       height = 5 * scl,
+       device = cairo_pdf)
+
+# ==============================
 # scaf_pick <- 12:13
 #
 # psl_diag_sub <- psl_filtered |>
