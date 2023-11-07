754cdd62d5c536fe824fa3d3f47085804bcb0b16
k-hench <khench@posteo.net>
2023-11-07 15:58:02 +0100

    bug

--- R/reroot_tree.R
+++ R/reroot_tree.R
@@ -5,7 +5,7 @@ args <- commandArgs(trailingOnly = TRUE)
 tree <- ape::read.tree(file = args[[1]])
 tree_rerooted <- reroot(tree, node.number = as.numeric(args[[2]]))
 
-tree_rotated <- tree_rerooted |>
+tree_rotated <- tree_rerooted
 
 ape::write.tree(phy = purrr::reduce(c(12, 18, 19, 20, 13, 14, 21, 17),
                                     ape::rotate,
