8aa46dc75526d2cd56024995fc378e29cb51ad35
k-hench <khench@posteo.net>
2023-11-07 15:49:47 +0100

    bugfix

--- R/reroot_tree.R
+++ R/reroot_tree.R
@@ -6,5 +6,5 @@ tree <- ape::read.tree(file = args[[1]])
 tree_rerooted <- reroot(tree, node.number = as.numeric(args[[2]]))
 ape::write.tree(phy = purrr::reduce(c(12, 18, 19, 20, 13, 14, 21, 17),
                                     ape::rotate,
-                                    .init = tree_rerooted)),
+                                    .init = tree_rerooted),
                 file = args[[3]], digits = 6)
