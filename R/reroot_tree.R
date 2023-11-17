e0a9fefbac37382053551ec1549282fc175ff67e
k-hench <khench@posteo.net>
2023-11-17 10:17:04 +0100

    update tree

--- R/reroot_tree.R
+++ R/reroot_tree.R
@@ -5,9 +5,7 @@ args <- commandArgs(trailingOnly = TRUE)
 tree <- ape::read.tree(file = args[[1]])
 tree_rerooted <- reroot(tree, node.number = as.numeric(args[[2]]))
 
-tree_rotated <- tree_rerooted
-
-ape::write.tree(phy = purrr::reduce(c(21, 17),
+ape::write.tree(phy = purrr::reduce(c(12,18,21,17,19,20,21,14,15),
                                     ape::rotate,
                                     .init = tree_rerooted),
                 file = args[[3]], digits = 6)
